5878e91e5a43c105204ba45bebaf5bc96e59d215
legokay11 <kaytrein@gmail.com>
2016-06-14 21:03:53 +0200

    ggjh
    khj

---
+++ Game/Assets/Scripts/FPS/FPS.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FPS : MonoBehaviour
+{
+
+    public float updateInterval = 0.5F;
+
+    Text text;
+
+    private float accum = 0;
+    private int frames = 0;
+    private float timeleft;
+
+
+    void Start()
+    {
+        if (!GetComponent<Text>())
+        {
+            Debug.Log("UtilityFramesPerSecond needs a GUIText component!");
+            enabled = false;
+            return;
+        }
+        timeleft = updateInterval;
+    }
+
+
+    void Update()
+    {
+        timeleft -= Time.deltaTime;
+        accum += Time.timeScale / Time.deltaTime;
+        ++frames;
+
+        if (timeleft <= 0.0)
+        {
+            float fps = accum / frames;
+            string format = System.String.Format("{0:F2} FPS", fps);
+            GetComponent<Text>().text = format;
+
+            if (fps < 30)
+                GetComponent<Text>().color = Color.yellow;
+            else
+                if (fps < 10)
+                GetComponent<Text>().color = Color.red;
+            else
+                GetComponent<Text>().color = Color.green;
+            timeleft = updateInterval;
+            accum = 0.0F;
+            frames = 0;
+        }
+    }
+}
